bce7c562678f38f0724a6f6c7d8f648c2368c8f2
AriSinai <32492340+AriSinai@users.noreply.github.com>
2018-04-27 19:18:06 -0500

    Add files via upload

---
+++ Membresia.java
@@ -0,0 +1,24 @@
+public class Membresia{
+	
+
+	private String nombre;
+
+
+	public Membresia(){
+		this.nombre=nombre;
+		
+	}
+
+
+	public void setNombre(String nombre){
+		this.nombre=nombre;
+
+	}
+
+	public String getNombre(){
+		return nombre;
+	}
+
+
+
+}
---
+++ MembresiaBlack.java
@@ -0,0 +1,5 @@
+public class MembresiaBlack extends Membresia{
+	
+	
+
+}
